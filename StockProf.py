1e26f91a627d7eeaa13cfcf1ddee29889bf2c0a7
Anton <antonnrgrd@hotmail.com>
2023-08-26 09:51:12 +0200

    implemented basic report generation

--- StockProf.py
+++ StockProf.py
@@ -5,16 +5,74 @@ import requests as r
 import re
 import json
 import numpy as np
+from functools import reduce
 ex_rate_regex = "(Converted\sto<\/label><div>)([0-9\.+]+)(<)"
+
+
 class StockProfiler:
     def __init__(self):
+        counter = 1
+        self.reference_currency = None
         self.conversion_factors = {}
         self.regions = None
         present_date = datetime.datetime.now()
         present_date = present_date.strftime("%d-%m-%Y")
-        self.title = f"Portfolio report dated { present_date}"
-        self.counter = 1
+        self.title = f"Portfolio_report_dated_{present_date}"
+        self.present_date = present_date
+        userhome = os.path.expanduser('~') 
+        if os.path.exists("{}\\stockscraper_config\\{}.tex".format(userhome, self.title)):
+            self.title = f"Portfolio_report_dated_{present_date}({counter})"
+            while os.path.exists("{}\\stockscraper_config\\{}.tex".format(userhome, self.title)):
+                counter = counter + 1
+                self.title = f"Portfolio_report_dated_{present_date}({counter})"
+                                  
+
+  
+    def profiler_write_preamble(self, latex_report):
+        latex_report.write(
+          """
+        \\documentclass{article}
+        \\usepackage{graphicx} % Required for inserting images
+        \\usepackage{tikz}
+        \\usetikzlibrary{positioning,arrows}
+        \\usetikzlibrary{arrows.meta}
+        \\title{Portfolio report}
+        \\usepackage{pgf-pie}  
+        \\usepackage{subcaption}
+        \\date{Dated """ +  self.present_date + """ } """ 
+        +
+        
+        """
+        \\begin{document}
+        \\maketitle
+        \\tableofcontents   
+        """)  
+          
+           
+    def profiler_write_end(self, latex_report):
+        latex_report.write("""\\end{document} """)
+    def profiler_write_currency_conversion_info(self,latex_report):
+        reference_currency = self.reference_currency
+        
+        
+        latex_report.write("""\\begin{{table}}[!h]
+                            \\begin{{center}}
+                            \\begin{{tabular}}{{||c ||}} 
+                             \hline
+                             Reference currency : {}  \\\ [0.5ex] 
+                             \hline\hline
+                          """.format(reference_currency))
+        for currency, factor in self.conversion_factors:
+            latex_report.write("""{}:{}  \\\ 
+                               \hline """.format(currency, factor))
+        latex_report.write("""
+                           \\end{tabular}
+                            \\caption{{Conversion factors for reference currency.}}
+                            \\end{center}
+                            \\end{table}
+                           """)
     def profiler_derive_information(self, tickers,reference_currency):
+        self.reference_currency = reference_currency
         currencies = tickers.currency.unique()
         '''We are not interested in the conversion ratio for the reference currency, so we 
         disregard the reference currency, if there is any'''
@@ -23,23 +81,15 @@ class StockProfiler:
             conversion_factor_text = r.get(f"https://wise.com/us/currency-converter/{currency}-to-{reference_currency}-rate?amount=1").text
             factor = float(re.search(ex_rate_regex,conversion_factor_text).group(2).replace(",",""))
             self.conversion_factors[currency]=factor
-
+        ''' '''
+        self.conversion_factors[reference_currency] = 1
+    def profiler_get_as_ref_currency(self, stock_info):
+        if stock_info["currency"] in self.conversion_factors:
+            return stock_info["currency_amount"] * self.conversion_factors[stock_info["currency"]]
+        else:
+            return stock_info["currency_amount"]
     def profiler_write_report(self):
         pass
-    def profiler_write_preamble(self,latex_report):
-        latex_report.write("""\\documentclass{article}
-        \\usetikzlibrary{positioning,arrows} 
-        \\usepackage{tikz}                   
-        \\begin{document}
-        \\begin{titlepage}
-           \\vspace*{\stretch{1.0}}
-           \\begin{center}
-              \\Large\textbf{Stock portfolio report dated}\\
-              \\large\textit{A. Thor}
-           \\end{center}
-           \\vspace*{\stretch{2.0}}
-        \\end{titlepage}
-        """)
         
         
     def profiler_write_biggest_earner_loser(self,latex_report):
@@ -65,17 +115,17 @@ class StockProfiler:
         """)
         
     def profiler_write_ending(self,latex_report):
-         latex_report.write(""""\end{document}""")
+         latex_report.write("""\end{document}""")
    # @classmethod
     def profiler_generate_report_info(self):
         userhome = os.path.expanduser('~')          
-        user = os.path.split(userhome)[-1]
         if os.path.isfile(f"{userhome}\\stockscraper_config\\items.csv"):
             tickers =  pd.read_csv(f"{userhome}\\stockscraper_config\\items.csv")
             with open(f"{userhome}\\stockscraper_config\\config_info.json") as f:
                 config_info = json.load(f)
                 self.profiler_derive_information(tickers, config_info["ref_currency"])
             tickers['currency_amount'] = tickers.holding * tickers.current_price
+            
             currency_holdings = tickers.groupby(['currency'])['currency_amount'].sum().reset_index()
             '''We convert the target currency to the reference currency e.g DKK. If it is not present in the conversion factors, we assume
             that it is because it is the reference currency and so return it as so.'''
@@ -85,10 +135,35 @@ class StockProfiler:
 
     
             sector_holdings = tickers.groupby(['sector','currency'])['currency_amount'].sum().reset_index()
+            sector_holdings["currency_amount"] = sector_holdings["currency"].map(self.conversion_factors).mul(sector_holdings["currency_amount"]) 
+            sector_holdings = sector_holdings.rename(columns={'currency_amount': 'own_currency'})
+            sector_weighting = sector_holdings.groupby(['sector'])['own_currency'].sum().reset_index()
+            sector_weighting["weighting"] = sector_weighting["own_currency"] / total_holding_own_currency
+
+            
+
+
+            
             industry_holdings = tickers.groupby(['industry','currency'])['currency_amount'].sum().reset_index()
-            print(industry_holdings)
+            industry_holdings["currency_amount"] = industry_holdings["currency"].map(self.conversion_factors).mul(industry_holdings["currency_amount"]) 
+            industry_holdings = industry_holdings.rename(columns={'currency_amount': 'own_currency'})
+            industry_weighting = industry_holdings.groupby(['industry'])['own_currency'].sum().reset_index()
+            industry_weighting["weighting"] = industry_weighting["own_currency"] / total_holding_own_currency
+
+            country_holdings = tickers.groupby(['country','currency'])['currency_amount'].sum().reset_index()
+            country_holdings["currency_amount"] = country_holdings["currency"].map(self.conversion_factors).mul(country_holdings["currency_amount"]) 
+            country_holdings = country_holdings.rename(columns={'currency_amount': 'own_currency'})
+            country_weighting = country_holdings.groupby(['country'])['own_currency'].sum().reset_index()
             
             tickers['returns'] = tickers.initial_price / tickers.current_price
             biggest_return_p =  tickers.loc[tickers['returns'].idxmax()]
             biggest_loss_return_p =  tickers.loc[tickers['returns'].idxmin()]
             
+            userhome = os.path.expanduser('~') 
+            with open("{}\\stockscraper_config\\{}.tex".format(userhome, self.title), 'w') as report:
+                self.profiler_write_preamble(report)
+                self.profiler_write_ending(report)
+                 
+
+
+                     
